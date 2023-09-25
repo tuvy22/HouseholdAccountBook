886b679b4e115377efa358ed1e5441fdd0545ebd
yoshie <yyoshie313@gmail.com>
2023-09-25 19:34:51 +0900

    ログインボタンの変更

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { Button } from "@material-tailwind/react";
 
 const Login = () => {
   const [userId, setUserId] = useState('');
@@ -44,9 +45,11 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 className="mb-4 w-full px-3 py-2 border rounded"
             />
-            <button type="submit" className="w-full py-2 bg-blue-600 text-white rounded" disabled={loading}>
-                {loading ? 'Logging in...' : 'Login'}
-            </button>
+            <div className="flex justify-end"> 
+              <Button type="submit" variant="filled" color="green" size="md" className="mt-4 w-full md:w-auto">
+                Login
+              </Button>
+            </div>
         </form>
         {error && <div className="text-red-500 text-center mt-4">{error}</div>}
       </div>
