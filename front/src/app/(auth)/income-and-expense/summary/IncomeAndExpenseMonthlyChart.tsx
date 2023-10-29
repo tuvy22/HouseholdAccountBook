727decbcec67820206d365290f14451164cd13ed
yoshie <yyoshie313@gmail.com>
2023-10-29 18:00:36 +0900

    ログインに目のアイコン追加＋ダッシュボードに円グラフ追加

--- front/src/app/(auth)/income-and-expense/summary/IncomeAndExpenseMonthlyChart.tsx
+++ front/src/app/(auth)/income-and-expense/summary/IncomeAndExpenseMonthlyChart.tsx
@@ -11,39 +11,49 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { CategoricalChartState } from "recharts/types/chart/generateCategoricalChart";
 
-export const IncomeAndExpenseMonthlyChart = async ({
+export const IncomeAndExpenseMonthlyChart = ({
   data,
 }: {
   data: IncomeAndExpenseMonthlyTotal[];
 }) => {
+  const handlePointClick = (data: CategoricalChartState) => {
+    console.log(data.activeLabel);
+  };
+
   return (
     <>
-      <h2 className="mb-4 text-xl font-bold">月別推移</h2>
-      <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
-        <div className="absolute top-0 left-0 w-full h-full">
-          <ResponsiveContainer>
-            <LineChart
-              data={data}
-              margin={{ top: 20, right: 10, left: 10, bottom: 10 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="yearMonth" />
-              <YAxis tickFormatter={(value) => `${value}円`} />
+      {/* <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
+      <div className="absolute top-0 left-0 w-full h-full"> */}
+      <ResponsiveContainer height={400}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 10, left: 10, bottom: 10 }}
+          onClick={handlePointClick}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="yearMonth" dy={15} />
+          <YAxis tickFormatter={(value) => `${value}円`} dx={-15} />
 
-              <Tooltip />
-              <Legend />
-              <Line
-                type="linear"
-                dataKey="totalAmount"
-                stroke="#67b7dc"
-                activeDot={{ r: 8 }}
-                name="残高"
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
+          <Tooltip />
+          <Legend
+            verticalAlign="top"
+            align="right"
+            wrapperStyle={{ marginTop: -20, marginRight: -10 }}
+          />
+
+          <Line
+            type="linear"
+            dataKey="totalAmount"
+            stroke="#67b7dc"
+            activeDot={{ r: 8 }}
+            name="残高"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      {/* </div>
+    </div> */}
     </>
   );
 };
--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -13,6 +13,8 @@ import {
 import { useUser } from "@/app/context/UserProvider";
 import LockIcon from "@mui/icons-material/Lock";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { auth } from "../util/api";
 
 interface IFormInput {
@@ -23,6 +25,8 @@ interface IFormInput {
 const Login = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => setShowPassword(!showPassword);
   const router = useRouter();
   const { setUser } = useUser();
   const {
@@ -48,13 +52,13 @@ const Login = () => {
   };
 
   return (
-    <Card className="w-full max-w-[22rem]">
+    <Card className="w-full max-w-sm">
       <Typography variant="h2" className="text-center pt-5 text-xl">
         ログイン
       </Typography>
       <CardBody>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
-          <div className="flex items-center gap-3">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-[auto_1fr_auto] grid-rows[auto_auto_10px_auto] items-center gap-4">
             <AccountBoxIcon />
             <Input
               label="ユーザID"
@@ -62,31 +66,36 @@ const Login = () => {
               variant="standard"
               size="lg"
               crossOrigin={undefined}
-              icon={<i className="fas fa-heart" />}
               {...register("id")}
             />
-          </div>
-          <div className="flex items-center gap-3">
+            <div></div>
             <LockIcon />
             <Input
               label="パスワード"
-              type="password"
+              type={showPassword ? "text" : "password"}
               variant="standard"
               size="lg"
               crossOrigin={undefined}
-              icon={<i className="fas fa-heart" />}
               {...register("password")}
             />
+            <div className="text-xs text-gray-500 cursor-pointer hover:text-green-500">
+              {showPassword ? (
+                <VisibilityIcon onClick={toggleShowPassword} />
+              ) : (
+                <VisibilityOffIcon onClick={toggleShowPassword} />
+              )}
+            </div>
+            <div className="col-span-3"></div>
+            <Button
+              type="submit"
+              variant="filled"
+              color="green"
+              size="lg"
+              className="col-span-3"
+            >
+              {loading ? <Spinner className="m-auto" /> : <>ログイン</>}
+            </Button>
           </div>
-          <Button
-            type="submit"
-            variant="filled"
-            color="green"
-            size="lg"
-            className="mt-6 w-full"
-          >
-            {loading ? <Spinner className="m-auto" /> : <>ログイン</>}
-          </Button>
         </form>
         {error && <div className="text-red-500 text-center mt-4">{error}</div>}
       </CardBody>
