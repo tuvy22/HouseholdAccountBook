a74682fd20b207296f360042ce9ff2925df9c55e
yoshie <yyoshie313@gmail.com>
2023-11-03 11:48:09 +0900

    微調整

--- front/src/app/(auth)/income-and-expense/list/page.tsx
+++ front/src/app/(auth)/income-and-expense/list/page.tsx
@@ -1,6 +1,9 @@
+import { Metadata } from "next";
 import { IncomeAndExpenseForm } from "./IncomeAndExpenseForm";
 import { IncomeAndExpenseTable } from "./IncomeAndExpenseTable";
-
+export const metadata: Metadata = {
+  title: "家計簿一覧",
+};
 const Page = () => (
   <>
     <IncomeAndExpenseForm />
