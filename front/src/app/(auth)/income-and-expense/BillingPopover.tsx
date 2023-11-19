5e803e567b2317fc9a9230c8396a09480339ebb8
yoshie <yyoshie313@gmail.com>
2023-11-19 14:29:59 +0900

    BillingPopoverのコンポーネント化

---
+++ front/src/app/(auth)/income-and-expense/BillingPopover.tsx
@@ -0,0 +1,94 @@
+import {
+  Card,
+  Popover,
+  PopoverContent,
+  PopoverHandler,
+  Typography,
+} from "@/app/materialTailwindExports";
+import React from "react";
+import GroupIcon from "@mui/icons-material/Group";
+import Person from "@mui/icons-material/Person";
+import { IncomeAndExpense } from "@/app/util/types";
+
+const TABLE_INFO_BULLING = [
+  { header: "支払", addClassName: "" },
+  { header: "金額", addClassName: "" },
+  { header: "清算", addClassName: "" },
+];
+
+export default function BillingPopover({
+  incomeAndExpense,
+}: {
+  incomeAndExpense: IncomeAndExpense;
+}) {
+  return (
+    <Popover>
+      <PopoverHandler>
+        {incomeAndExpense.billingUsers.length == 1 ? (
+          <Person className="cursor-pointer hover:text-red-500" />
+        ) : (
+          <GroupIcon className="cursor-pointer hover:text-red-500" />
+        )}
+      </PopoverHandler>
+      <PopoverContent>
+        <Card className="h-full w-full">
+          <table className="max-w-full table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_INFO_BULLING.map((info, index) => (
+                  <th
+                    key={index}
+                    className={`border-b border-blue-gray-100 bg-blue-gray-50 p-4 ${info.addClassName}`}
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {info.header}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {incomeAndExpense.billingUsers.map(
+                (billingUser, index: number) => (
+                  <tr key={index} className="break-all">
+                    <td className="p-2 md:p-4 border-b border-blue-gray-50">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {billingUser.userID}
+                      </Typography>
+                    </td>
+                    <td className="p-2 md:p-4 border-b border-blue-gray-50">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal text-red-500"
+                      >
+                        {`${billingUser.amount}円`}
+                      </Typography>
+                    </td>
+                    <td className="p-2 md:p-4 border-b border-blue-gray-50">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {billingUser.liquidationFg ? "済" : "未"}
+                      </Typography>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </Card>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- front/src/app/(auth)/income-and-expense/IncomeAndExpenseTable.tsx
+++ front/src/app/(auth)/income-and-expense/IncomeAndExpenseTable.tsx
@@ -2,23 +2,11 @@ import { MemoPopover } from "@/app/components/MemoPopover";
 import { IncomeAndExpense } from "@/app/util/types";
 import { isMinus, toDateString } from "@/app/util/util";
 import React from "react";
-import {
-  Button,
-  Card,
-  Menu,
-  MenuHandler,
-  MenuItem,
-  MenuList,
-  Popover,
-  PopoverContent,
-  PopoverHandler,
-  Typography,
-} from "@/app/materialTailwindExports";
+import { Card, Typography } from "@/app/materialTailwindExports";
 import EditIcon from "../../components/EditIcon";
 import DeleteIcon from "../../components/DeleteIcon";
 import { getIncomeAndExpense } from "@/app/util/apiServer";
-import GroupIcon from "@mui/icons-material/Group";
-import Person from "@mui/icons-material/Person";
+import BillingPopover from "./BillingPopover";
 
 const TABLE_INFO = [
   { header: "登録", addClassName: "" },
@@ -27,11 +15,6 @@ const TABLE_INFO = [
   { header: "金額", addClassName: "" },
   { header: "メモ", addClassName: "w-[40%]" },
   { header: "", addClassName: "w-28" },
-];
-const TABLE_INFO_BULLING = [
-  { header: "支払", addClassName: "" },
-  { header: "金額", addClassName: "" },
-  { header: "清算", addClassName: "" },
 ];
 
 export const IncomeAndExpenseTable = async () => {
@@ -101,86 +84,13 @@ export const IncomeAndExpenseTable = async () => {
                         </td>
                         <td className="p-2 md:p-4 border-b border-blue-gray-50">
                           <div>
-                            {}
                             {incomeAndExpense.billingUsers.length > 1 ||
                             (incomeAndExpense.billingUsers.length == 1 &&
                               incomeAndExpense.billingUsers[0].userID !==
                                 incomeAndExpense.registerUserId) ? (
-                              <Popover>
-                                <PopoverHandler>
-                                  {incomeAndExpense.billingUsers.length == 1 ? (
-                                    <Person className="cursor-pointer hover:text-red-500" />
-                                  ) : (
-                                    <GroupIcon className="cursor-pointer hover:text-red-500" />
-                                  )}
-                                </PopoverHandler>
-                                <PopoverContent>
-                                  <Card className="h-full w-full">
-                                    <table className="max-w-full table-auto text-left">
-                                      <thead>
-                                        <tr>
-                                          {TABLE_INFO_BULLING.map(
-                                            (info, index) => (
-                                              <th
-                                                key={index}
-                                                className={`border-b border-blue-gray-100 bg-blue-gray-50 p-4 ${info.addClassName}`}
-                                              >
-                                                <Typography
-                                                  variant="small"
-                                                  color="blue-gray"
-                                                  className="font-normal leading-none opacity-70"
-                                                >
-                                                  {info.header}
-                                                </Typography>
-                                              </th>
-                                            )
-                                          )}
-                                        </tr>
-                                      </thead>
-                                      <tbody>
-                                        {incomeAndExpense.billingUsers.map(
-                                          (billingUser, index: number) => (
-                                            <tr
-                                              key={index}
-                                              className="break-all"
-                                            >
-                                              <td className="p-2 md:p-4 border-b border-blue-gray-50">
-                                                <Typography
-                                                  variant="small"
-                                                  color="blue-gray"
-                                                  className="font-normal"
-                                                >
-                                                  {billingUser.userID}
-                                                </Typography>
-                                              </td>
-                                              <td className="p-2 md:p-4 border-b border-blue-gray-50">
-                                                <Typography
-                                                  variant="small"
-                                                  color="blue-gray"
-                                                  className="font-normal text-red-500"
-                                                >
-                                                  {`${billingUser.amount}円`}
-                                                </Typography>
-                                              </td>
-                                              <td className="p-2 md:p-4 border-b border-blue-gray-50">
-                                                <Typography
-                                                  variant="small"
-                                                  color="blue-gray"
-                                                  className="font-normal"
-                                                >
-                                                  {billingUser.liquidationFg
-                                                    ? "済"
-                                                    : "未"}
-                                                </Typography>
-                                              </td>
-                                            </tr>
-                                          )
-                                        )}
-                                      </tbody>
-                                    </table>
-                                  </Card>
-                                </PopoverContent>
-                              </Popover>
+                              <BillingPopover
+                                incomeAndExpense={incomeAndExpense}
+                              />
                             ) : (
                               <span>
                                 {incomeAndExpense.billingUsers.length === 1
